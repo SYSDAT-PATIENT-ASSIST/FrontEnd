86fad0932133b9545f7ad0f5fac558d321e338dd
Jeppe Koch <cph-jk469@cphbusiness.dk>
2025-05-05 12:36:59 +0200

    translate header and home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router';
+import { useTranslation } from 'react-i18next';
+import { Link } from "react-router";
 import Logo from '../assets/hospital-region-logo.jpeg';
 import QuitIcon from '../assets/exit-icon.webp';
 import SettingsIcon from '../assets/settings-icon-2.png';
@@ -6,55 +7,46 @@ import AdminButton from './auth/AdminButton';
 import '../styles/Header.css';
 
 function Header() {
+  const { t } = useTranslation(); // Access the translation function
+
   return (
     <>
       <div className="header__panel">
-
         <div className="header__panel-left">
-
           <Link to="/" className="header__panel-left-logo">
             <img src={Logo} className="logo" alt="Logo" />
-            <h1>Bornholms<br />Hospital</h1>
+            <h1>{t('headerHospitalName')}</h1> {/* Use translation keys here */}
           </Link>
-
+          
           <Link to="/" className="header__panel-left-location">
-            <h1>202-1</h1>
-            <h2>Kirurgisk afdeling</h2>
+            <h1>{t('headerLocationCode')}</h1> {/* Use translation keys here */}
+            <h2>{t('headerDepartmentName')}</h2> {/* Use translation keys here */}
           </Link>
-
         </div>
-
+        
         <div className="header__panel-right">
+        <AdminButton />
 
           <div className="header__panel-right-actions">
 
-            <div className='flex gap-2 h-full'>
-              <AdminButton />
+    
+            <Link to="/" className="header__panel-right-actions-quit"> 
+              <img src={QuitIcon} className="logo" alt={t('quit')} /> {/* Dynamic alt text */}
+            </Link>
 
-              <div className='header__panel-right-actions'>
-
-                <Link to="/" className="header__panel-right-actions-quit">
-                  <img src={QuitIcon} className="logo" alt="Quit" />
-                </Link>
-
-                <Link to="/Settings" className="header__panel-right-actions-settings">
-                  <img src={SettingsIcon} className="logo" alt="Settings" />
-                </Link>
-
-              </div>
-            </div>
-
+            <Link to="/Settings" className="header__panel-right-actions-settings"> 
+              <img src={SettingsIcon} className="logo" alt={t('settings')} /> {/* Dynamic alt text */}
+            </Link>
           </div>
-
+          
           <Link to="/calendar" className="header__panel-right-date_time">
-            <h1>Mandag 10-03</h1>
-            <h2>15:00</h2>
+            <h1>{t('headerDay')}</h1> {/* Use translation keys here */}
+            <h2>{t('headerTime')}</h2> {/* Use translation keys here */}
           </Link>
-
         </div>
-
       </div>
     </>
-  )
+  );
 }
+
 export default Header;
