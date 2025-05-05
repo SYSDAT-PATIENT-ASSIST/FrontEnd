b9094bcc3202570f842470f15f1100b956c80289
DeZoete <143606680+DeZoete@users.noreply.github.com>
2025-05-05 10:38:54 +0200

    Opdateret homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import { NavLink, Link, Outlet } from "react-router"
+import { useTranslation } from 'react-i18next'
+import { Link } from "react-router"
 import Logo from '../assets/HospitalLogo.jpg'
 import '../styles/Home.css'
-import Header from '../components/Header.jsx'
-import { useSettings } from '../components/SettingsContext.jsx'
 
-function Home() 
-{
+function Home() {
+  const { t } = useTranslation('home') // 'home' = namespace
+
   return (
     <>
       <div className="home__container">
-
-      <div className="home__links">
-      
-        <Link to="/calendar"> <img src={Logo} /> <h1>Mad bestilling</h1> </Link>
-        <Link to="/calendar"> <img src={Logo} /> <h1>Øvelser</h1> </Link>
-        <Link to="/calendar"> <img src={Logo} /> <h1>Kontakt</h1> </Link>
-        <Link to="/calendar"> <img src={Logo} /> <h1>Hospital info</h1> </Link>
-        <Link to="/calendar"> <img src={Logo} /> <h1>Underholdning</h1> </Link>
-        <Link to="/calendar"> <img src={Logo} /> <h1>Min SP</h1> </Link>
-      
-      </div>
-        
-      
+        <div className="home__links">
+          <Link to="/calendar"> <img src={Logo} /> <h1>{t('menuMealOrder')}</h1> </Link>
+          <Link to="/calendar"> <img src={Logo} /> <h1>{t('menuExercises')}</h1> </Link>
+          <Link to="/calendar"> <img src={Logo} /> <h1>{t('menuContact')}</h1> </Link>
+          <Link to="/calendar"> <img src={Logo} /> <h1>{t('menuHospitalInfo')}</h1> </Link>
+          <Link to="/calendar"> <img src={Logo} /> <h1>{t('menuEntertainment')}</h1> </Link>
+          <Link to="/calendar"> <img src={Logo} /> <h1>{t('menuMySP')}</h1> </Link>
+        </div>
       </div>
     </>
   )
 }
 
 export default Home
-
-
