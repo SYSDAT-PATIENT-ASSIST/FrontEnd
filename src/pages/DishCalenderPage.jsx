70dd57c3b695a304a304cd363b32926ae72aa1b6
Jeppe Koch <cph-jk469@cphbusiness.dk>
2025-05-07 12:19:14 +0200

    Dish translated

--- src/pages/DishCalenderPage.jsx
+++ src/pages/DishCalenderPage.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import CalendarView from "../components/CalendarView";
 import DishSelector from "../components/DishSelector";
+import { useTranslation } from "react-i18next";
 import "../styles/DishCalenderPage.css";
 
 export default function DishCalendarPage() {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const { t } = useTranslation();
 
   return (
     <div className="team-e__dish-calendar-page">
-      <h1 className="team-e__title">Dish Calendar Manager</h1>
+      <h1 className="team-e__title">{t("dishCalendarManagerTitle")}</h1>
       <div className="team-e__calendar-grid">
         <DishSelector selectedDate={selectedDate} />
         <CalendarView selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
