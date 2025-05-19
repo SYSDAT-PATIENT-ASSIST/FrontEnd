3d6a28bf6cca7c830d16fb1149fa66932917a134
Marcus Rasmussen <marcusphr1808@gmail.com>
2025-05-19 14:16:52 +0200

    List of pations who has not yet ordered

---
+++ src/pages/MissingOrders.jsx
@@ -0,0 +1,30 @@
+const MissingOrders = () => {
+
+
+    // Dummy data for patients
+    const patient = [
+        { firstName: "Rasmussen", bed: "201" }, 
+        { firstName: "Henriksen", bed: "202" }, 
+        { firstName: "Christiansen", bed: "203" }, 
+        { firstName: "Christiansen", bed: "204" },
+        { firstName: "Christiansen", bed: "205" }, 
+        { firstName: "Christiansen", bed: "206" }
+    ];
+
+
+    return ( 
+        <div className="bg-white rounded-2xl p-4 mt-4 md:w-[40rem] text-black m-auto w-[20rem] h-[30rem] overflow-y-scroll">
+            <h1 className="font-semibold text-2xl mb-2">Patienter der mangler mad</h1>
+            <div className="flex flex-col gap-2">
+                {patient.map((p, index) => (
+                    <div key={index} className="flex items-center border-b-2 border-gray-300 p-4">
+                        <span className="bg-blue-200 flex items-center justify-center w-14 h-14 rounded-full mr-4 text-white">{p.bed}</span>
+                        <span className="text-black">{p.firstName}</span>
+                    </div>
+                ))}
+            </div>
+        </div>
+     );
+}
+ 
+export default MissingOrders;
