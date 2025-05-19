cea1c47056faf53fa3a73b1e8aa48e9cb04a04c3
Mattsigurd <matthias.sigurds@gmail.com>
2025-05-19 13:59:47 +0200

    MissingOrders knap til login siden

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -121,6 +121,16 @@ const LoginForm = () => {
           )}
         </button>
       </form>
+      <div className="mt-6 text-center">
+  <button
+    onClick={() => navigate('/MissingOrders')}
+    type="button"
+    className="inline-flex items-center justify-center px-4 py-2 border border-blue-600 text-blue-600 text-sm font-medium rounded-md hover:bg-blue-50 transition"
+  >
+    Gå til Manglende bestillinger
+  </button>
+</div>
+
     </div>
   );
 };
